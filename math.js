0e0d2c3e076059c0b62a520ba890f680548e4ab7
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-03 16:45:51 +0200

    modified the math export modules

--- math.js
+++ math.js
@@ -3,7 +3,16 @@ const add= (x, y) => x + y;
 const PI = 3.142;
 
 const square = x => x * x;
-
+/*
 module.exports.add = add;
 module.exports.PI = PI;
 module.exports.square = square;
+*/
+
+const math = {
+    add: add,
+    PI: PI,
+    square: square
+}
+
+module.exports = math;
