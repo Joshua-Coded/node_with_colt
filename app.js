137e543ec841a6bc4e5bf68a1a203c708ff2b9b4
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-03 16:36:38 +0200

    imported my personal module into my file in node.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const math = require("./math");
+console.log(math.PI);
+console.log(math.square(10));
+console.log(math.add(12, 43));
---
+++ math.js
@@ -0,0 +1,9 @@
+const add= (x, y) => x + y;
+
+const PI = 3.142;
+
+const square = x => x * x;
+
+module.exports.add = add;
+module.exports.PI = PI;
+module.exports.square = square;
