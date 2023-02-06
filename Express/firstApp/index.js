c55353cc66a86f1d18c93f6fb10fb7e8348a368b
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-06 20:35:59 +0200

    express res.send method

--- Express/firstApp/index.js
+++ Express/firstApp/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 
-app.use(() => {
+app.use((req, res) => {
     console.log("we got a new request");
+    res.send("hello world!!");
 })
-
 
 
 app.listen(4000, () => {
