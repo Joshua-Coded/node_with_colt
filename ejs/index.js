c304691740ab11dbac345f3c8ee434a029f36e6a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 21:01:38 +0200

    servinf files in public

--- ejs/index.js
+++ ejs/index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const redditData = require("./data.json");
 
+app.use(express.static(path.join(__dirname, "public")))
+
 
 
 app.set("view engine", "ejs");
