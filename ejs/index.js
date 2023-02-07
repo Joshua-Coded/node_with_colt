2fc49d1d675e4a4b37905fc2040a2f9c04419dbf
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 18:45:58 +0200

    views

---
+++ ejs/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("Hi")
+})
+
+app.listen(4000, () => {
+    console.log("port listening on port 4000!!");
+})
