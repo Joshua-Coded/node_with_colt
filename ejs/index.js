9d1a05a3481382b34d5655896b6b45b418cb47aa
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 19:00:44 +0200

    viewing path

--- ejs/index.js
+++ ejs/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home")
