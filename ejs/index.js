c0cd1213b147067b8a382119f1cab4dd7a703e43
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 20:47:08 +0200

    error with data.json file

--- ejs/index.js
+++ ejs/index.js
@@ -1,7 +1,10 @@
+const { red } = require("colors");
 const express = require("express");
-const { errors } = require("jshint/src/messages");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
+
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -19,7 +22,14 @@ app.get("/cats", (req, res) => {
 
 app.get("/r/:subreddit", (req, res) => {
     const {subreddit} = req.params;
-    res.render("subreddit", {subreddit});
+    const data = redditData[subreddit];
+    
+    if (data) {
+        res.render("subreddit", {...data});
+    } else {
+        res.render("notfound", {subreddit})
+    }
+    
 })
 
 
