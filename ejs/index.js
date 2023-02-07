85c84779ff04fe98f8f45049d203928e6299e7a2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 19:22:47 +0200

    templateig

--- ejs/index.js
+++ ejs/index.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
+app.get("/random", (req, res) => {
+    const num  = Math.floor(Math.random() * 10 )  + 1;
+    res.render("random", { num: num})
+})
+
 app.listen(4000, () => {
     console.log("port listening on port 4000!!");
 })
