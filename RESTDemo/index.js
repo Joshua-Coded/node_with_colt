3e51555ad29b02b562834920a6d882a61ca7a7b4
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-13 18:40:35 +0200

    restapi ejs

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//fake comments
+
+const comments = [
+    {
+        username: "jsojhua",
+        comment: "This is a comment"
+    },
+    {
+        username: "UBEPUBVUVIUEbwvuiw",
+        comment: "vodoINIDVWIVNIFBVI",
+    },
+    {
+        username: "DIVINE",
+        comment: "This is a svcuvPWEBUVUVUWVBWE",
+    },
+    {
+        username: "stephen",
+        comment: "memememememem comment",
+    },
+    {
+        username: "skyme",
+        comment: "lolololo a comment",
+    }
+]
+
+app.get("/comments", (req, res) => {
+    res.render("comments/index");
+})
 
 app.get("/tacos", (req, res)  =>{
        res.send("GET /tacos response")
@@ -20,3 +53,6 @@ app.get("/tacos", (req, res) => {
 app.listen(3000, () => {
     console.log("listening on port 3000@@@!")
 });
+
+
+
