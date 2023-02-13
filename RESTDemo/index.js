f5998dca40d354b151030dcdc9978858d8e3ef2d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-13 21:26:33 +0200

    show page

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -11,23 +11,28 @@ app.set('view engine', 'ejs');
 //fake comments
 
 const comments = [
-    {
+    { 
+        id: 1,
         username: "jsojhua",
         comment: "This is a comment"
     },
     {
+        id: 2,
         username: "UBEPUBVUVIUEbwvuiw",
         comment: "vodoINIDVWIVNIFBVI",
     },
     {
+        id: 3,
         username: "DIVINE",
         comment: "This is a svcuvPWEBUVUVUWVBWE",
     },
     {
+        id: 4,
         username: "stephen",
         comment: "memememememem comment",
     },
     {
+        id: 5,
         username: "skyme",
         comment: "lolololo a comment",
     }
@@ -37,17 +42,21 @@ app.get("/comments", (req, res) => {
     res.render("comments/app", { comments });
 })
 
-
 app.get("/comments/new", (req, res) => {
     res.render("comments/new");
 })
-
 
 app.post("/comments/", (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment })
     res.redirect("/comments");
 });
+
+app.get("/comments/:id", (req, res) => {
+const { id } = req.params;
+const comment = comments.find(c => c.id === parseInt(id))
+res.render("comments/show", { comment })
+})
 
 
 app.listen(3000, () => {
