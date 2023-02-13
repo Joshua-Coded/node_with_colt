f124e3e882602e24ba46a3da6b799447698087c2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-13 18:05:29 +0200

    get requests are for getting data back

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/tacos", (req, res)  =>{
-    res.send("GET /tacos response")
+       res.send("GET /tacos response")
 })
 
+
+app.post("/tacos", (req, res)  =>{
+    console.log(req.body)
+    res.send("GET /tacos response")
+})
 
 app.get("/tacos", (req, res) => {
     res.send("POST /tacos response")
