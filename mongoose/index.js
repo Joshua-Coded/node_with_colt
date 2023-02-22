e009db51927856fdb9e308dd5ef3d50b5e7889d3
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-22 04:07:44 +0200

    model insertMany

--- mongoose/index.js
+++ mongoose/index.js
@@ -15,9 +15,24 @@ const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
     score: Number,
-    rating: String,
+    rating: String, 
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
 
-const amadue = new Movie({title: "Joshua", year: 2023,  score: 4.5, rating: 'R'});
+// const amadue = new Movie({title: "Joshua", year: 2023,  score: 4.5, rating: 'R'});
+
+Movie.insertMany([
+    {title: "Adamue", year: 2012, score: 4.2, rating: 'R'},
+    {title: "Adamue", year: 2012, score: 4.2, rating: 'R'},
+    {title: "call me in", year: 2010, score: 7.2, rating: 'PG'},
+    {title: "take me there", year: 2012, score: 4.2, rating: 'R'},
+    {title: "john wick", year: 2019, score: 9.5, rating: 'PG'},
+    {title: "old adam", year: 2011, score: 6.2, rating: 'R'},
+    {title: "Me and You", year: 2015, score: 7.2, rating: 'PG'},
+    {title: "Ghost", year: 2022, score: 9.0, rating: 'PG'},
+])
+.then(d => {
+    console.log("IT WORKED!!!!!!!!");
+    console.log(d);
+})
