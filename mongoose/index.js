e2a0e91f62577b7c0438c833cd4cdaa99701aadf
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-22 03:55:30 +0200

    model schema

--- mongoose/index.js
+++ mongoose/index.js
@@ -10,3 +10,14 @@ then(() => {
     console.log(err);
 })
 
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+const amadue = new Movie({title: "Joshua", year: 2023,  score: 4.5, rating: 'R'});
