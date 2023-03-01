fea67e78c5f1f7d174b40c29f640eb93ef7339bf
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-03-01 04:52:52 +0200

    models

---
+++ mongoose_express/models/productModels.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// Schema declarations
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    category: {
+        type: String,
+        enum: ['fruit', 'vegetable', 'dairy']
+    }
+})
+
+// compile the model
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
